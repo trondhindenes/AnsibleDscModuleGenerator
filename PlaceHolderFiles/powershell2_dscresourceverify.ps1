d60bae1f1223453a708a01a35d9f56670e16366d
Trond Hindenes <trond@hindenes.com>
2015-05-27 23:33:07 +0200

    Coming along nicely. nicely!

---
+++ PlaceHolderFiles/powershell2_dscresourceverify.ps1
@@ -0,0 +1,21 @@
+#This code comes from powershell2_dscresourceverify.ps1 in the DSC-->Ansible codegen tool
+
+$AutoInstallModule = $AutoInstallModule | convertto-bool
+
+$ResourceExists = Get-DscResource $dscresourcename -ErrorAction SilentlyContinue
+
+if (!$ResourceExists)
+{
+    #Download the module containing the resource if that's allowed
+    if ($AutoInstallModule)
+    {
+        Find-Module $dscmodulename | install-module -Confirm:$false -Force
+    }
+    
+    $ResourceExists = Get-DscResource $dscresourcename -ErrorAction SilentlyContinue
+}
+
+if (!$ResourceExists)
+{
+    Fail-Json $result "Unable to locate DSC module $dscmodulename and/or DSC resource $dscresourcename"
+}
---
+++ PlaceHolderFiles/powershell3_dscparser.ps1
@@ -0,0 +1,172 @@
+$Attributes = $params | get-member | where {$_.MemberTYpe -eq "noteproperty"}  | select -ExpandProperty Name
+
+
+
+if (!($Attributes))
+{
+    Fail-Json -obj $result -message "No attributes specified"
+}
+
+#Always return the name
+set-attr -obj $result -name "resource_name" -value $dscresourcename
+set-attr -obj $result -name "Attributes" -value $Attributes
+
+$Attrib = @{}
+
+<#
+$params.Keys | foreach-object {
+    $Attrib.Add($_,$params.Item($_))
+    set-attr -obj $result -name $_ -value $params.Item($_)
+    }
+#>
+
+$Keys = $params.psobject.Properties | where {$_.MemberTYpe -eq "Noteproperty"} | where {$_.Name -ne "resource_name"} | select -ExpandProperty Name
+foreach ($key in $keys)
+{
+    $Attrib.add($key, ($params.$key))
+    set-attr -obj $result -name $key -value ($params.$key)
+}
+
+$Config = @{
+   Name = ($dscresourcename)
+   Property = @{
+        }
+    }
+
+
+$Resource = Get-DscResource -Name $resourcename -ErrorAction SilentlyContinue
+if (!$Resource)
+{
+    Fail-Json -obj $result -message "Resource $resourcename not found"
+}
+
+#Convert params to correct datatype and inject
+#Convert params to correct datatype and inject
+$attrib.Keys | foreach-object {
+    $Key = $_.replace("item_name", "name")
+    $prop = $resource.Properties | where {$_.Name -eq $key}
+    if (!$prop)
+    {
+        #If its a credential specified as "credential", Ansible will support credential_username and credential_password. Need to check for that
+        $prop = $resource.Properties | where {$_.Name -eq $key.Replace("_username","")}
+        if ($prop)
+        {
+            #We need to construct a cred object. At this point keyvalue is the username, so grab the password
+            $PropUserNameValue = $attrib.Item($_)
+            $PropPassword = $key.Replace("_username","_password")
+            $PropPasswordValue = $attrib.$PropPassword
+
+            $cred = New-Object System.Management.Automation.PSCredential ($PropUserNameValue, ($PropPasswordValue | ConvertTo-SecureString -AsPlainText -Force))
+            [System.Management.Automation.PSCredential]$KeyValue = $cred
+            $config.Property.Add($key.Replace("_username",""),$KeyValue)
+        }
+        ElseIf ($key.Contains("_password"))
+        {
+            #Do nothing. We suck in the password in the handler for _username, so we can just skip it.
+        }
+        Else
+        {
+            Fail-Json -obj $result -message "Property $key in resource $resourcename is not a valid property"
+        }
+        
+    }
+    ElseIf ($prop.PropertyType -eq "[string]")
+    {
+        [String]$KeyValue = $attrib.Item($_)
+        $config.Property.Add($key,$KeyValue)
+    }
+    ElseIf ($prop.PropertyType -eq "[string[]]")
+    {
+        #KeyValue is an array of strings
+        [String]$TempKeyValue = $attrib.Item($_)
+        [String[]]$KeyValue = $TempKeyValue.Split(",").Trim()
+
+        $config.Property.Add($key,$KeyValue)
+    }
+    ElseIf ($prop.PropertyType -eq "[UInt32[]]")
+    {
+        #KeyValue is an array of integers
+        [String]$TempKeyValue = $attrib.Item($_)
+        [UInt32[]]$KeyValue = $attrib.Item($_.split(",").Trim())
+        $config.Property.Add($key,$KeyValue)
+    }
+    ElseIf ($prop.PropertyType -eq "[bool]")
+    {
+        if ($attrib.Item($_) -like "true")
+        {
+            [bool]$KeyValue = $true
+        }
+        ElseIf ($attrib.Item($_) -like "false")
+        {
+            [bool]$KeyValue = $false
+        }
+        $config.Property.Add($key,$KeyValue)
+    }
+    ElseIf ($prop.PropertyType -eq "[int]")
+    {
+        [int]$KeyValue = $attrib.Item($_)
+        $config.Property.Add($key,$KeyValue)
+    }
+    ElseIf ($prop.PropertyType -eq "[CimInstance[]]")
+    {
+      #KeyValue is an array of CimInstance
+      [CimInstance[]]$KeyVal = @()
+      [String]$TempKeyValue = $attrib.Item($_)
+      #Need to split on the string }, because some property values have commas in them
+      [String[]]$KeyValueStr = $TempKeyValue -split("},")
+      #Go through each string of properties and create a hash of them
+      foreach($str in $KeyValueStr)
+      {
+        [string[]]$properties = $str.Split("{")[1].Replace("}","").Trim().Split([environment]::NewLine).Trim()
+        $prph = @{}
+        foreach($p in $properties)
+        {
+          $pArr = $p -split "="
+          #if the value can be an int we must convert it to an int
+          if([bool]($pArr[1] -as [int] -is [int]))
+          {
+              $prph.Add($pArr[0].Trim(),$pArr[1].Trim() -as [int])
+          }
+          else
+          {
+              $prph.Add($pArr[0].Trim(),$pArr[1].Trim())
+          }
+        }
+        #create the new CimInstance
+        $cim = New-CimInstance -ClassName $str.Split("{")[0].Trim() -Property $prph -ClientOnly
+        #add the new CimInstance to the array
+        $KeyVal += $cim
+      }
+      $config.Property.Add($key,$KeyVal)
+    }
+  }
+
+try
+{
+    $TestResult = Invoke-DscResource @Config -Method Test -ErrorVariable TestError -ErrorAction SilentlyContinue
+    if (!($TestResult))
+    {
+       throw ($TestError[0].Exception.Message)
+    }
+    ElseIf (($testResult.InDesiredState) -ne $true) 
+    {
+        Invoke-DscResource -Method Set @Config -ErrorVariable SetError -ErrorAction SilentlyContinue
+        Set-Attr $result "changed" $true
+        if ($SetError)
+        {
+           throw ($SetError[0].Exception.Message)
+        }
+    }
+
+}
+Catch
+{
+    Fail-Json -obj $result -message $_[0].Exception.Message
+}
+
+
+#set-attr -obj $result -name "property" -value $property
+Exit-Json -obj $result
+
+
+
