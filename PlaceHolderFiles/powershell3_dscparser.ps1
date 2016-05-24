dc800bbf7a6b98fa81e48b8117aab21c3a8e9f79
Trond Hindenes <trond@hindenes.com>
2016-05-24 20:13:16 +0200

    CheckMode

--- PlaceHolderFiles/powershell3_dscparser.ps1
+++ PlaceHolderFiles/powershell3_dscparser.ps1
@@ -22,6 +22,14 @@ $params.Keys | foreach-object {
     }
 #>
 
+$CheckMode = $False
+$CheckFlag = $params.psobject.Properties | where {$_.Name -eq "_ansible_check_mode"}
+if ($CheckFlag)
+{
+    if ($CheckFlag | convertto-Bool -eq $True)
+    $CheckMode = $True
+}
+
 $Keys = $params.psobject.Properties | where {$_.MemberTYpe -eq "Noteproperty"} | where {$_.Name -ne "resource_name"} |where {$_.Name -ne "autoinstallmodule"} |where {$_.Name -ne "autoconfigurelcm"} | where {$_.Name -notlike "_ansible*"} |  select -ExpandProperty Name
 foreach ($key in $keys)
 {
